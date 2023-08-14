5346e771e363ebc264bcdb04b463a3c5cf95e142
Garrett Chavez <garrettachavez@gmail.com>
2023-08-14 13:05:11 -0700

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dropdownButtons = document.querySelectorAll(".cres-nav button:not(:last-child)");
+    const primaryLinks = document.querySelectorAll(".primary-links");
+
+    let currentOpenDropdown = null;
+
+    function closeDropdowns() {
+        dropdownButtons.forEach(button => {
+            const dropdown = button.nextElementSibling;
+            dropdown.classList.remove("open");
+            button.classList.remove("open");
+        });
+        currentOpenDropdown = null;
+    }
+
+    function toggleDropdown(button, index) {
+        const dropdown = primaryLinks[index];
+
+        if (currentOpenDropdown === dropdown) {
+            closeDropdowns();
+        } else {
+            closeDropdowns();
+            dropdown.classList.add("open");
+            button.classList.add("open");
+            currentOpenDropdown = dropdown;
+        }
+    }
+
+    dropdownButtons.forEach((button, index) => {
+        button.addEventListener("click", event => {
+            event.stopPropagation();
+            toggleDropdown(button, index);
+        });
+    });
+
+    document.addEventListener("click", event => {
+        const target = event.target;
+        if (!target.closest(".cres-nav")) {
+            closeDropdowns();
+        }
+    });
+
+    const asides = document.querySelectorAll(".card-dropdown aside");
+    asides.forEach(aside => {
+        aside.addEventListener("click", () => {
+            aside.classList.toggle("open");
+            const svg = aside.querySelector("svg");
+            svg.classList.toggle("open");
+            const hiddenContent = aside.querySelector(".hidden");
+            hiddenContent.classList.toggle("open");
+        });
+    });
+});
